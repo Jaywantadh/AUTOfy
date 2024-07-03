077804423c881f2667934ddc3b07228f1029eb35
Jaywant Adhau <119965656+Jaywantadh@users.noreply.github.com>
2024-07-03 23:24:38 +0530

    Update AUTOfy_Client.py

--- AUTOfy_Client.py
+++ AUTOfy_Client.py
@@ -20,4 +20,62 @@ class SpotifyClient:
             token_info = self.auth_manager.refresh_access_token(token_info['refresh_token'])
         return token_info['access_token']
     
+    def search_and_queue_song(self, song_name):
+        try:
+            results = self.sp.search(q = song_name, type='track', limit=1)
+            if results['tracks']['items']:
+                track = results['tracks']['items'][0]
+                track_uri = track['uri']
+                self.sp.add_to_queue(track_uri)
+                return track['name']
+            return None
+        except SpotifyException as e:
+            print(f"Spotify API Error: {e}")
+            return None
+        except Exception as e:
+            print("Unexpected Error occurred: {e}")
+            return None
     
+    def get_current_playback(self):
+        try:
+            playback = self.sp.current_playback()
+            if playback and playback['is_playing']:
+                return ['playback']['name']
+            return "No song is currently playing"
+        except SpotifyException as e:
+            print(f"Spotify API Error: {e}")
+            return None
+        except Exception as e:
+            print(f"Unexpected error occured: {e}")
+            return None
+        
+    def conmtrol_playback(self, action):
+        try:
+            if action == "play":
+                self.sp.start_playback()
+            elif action == "pause":
+                self.sp.pause_playback()
+            elif action == "next":
+                self.sp.next_playback()
+            elif action == "previous":
+                self.sp.previous_playback()
+        except SpotifyException as e:
+            print(f"Spotfy API Error: {e}")
+            return None
+        except Exception as e:
+            print(f"Unexpected error occured: {e}")
+            return None
+        
+    def create_playlist(self, playlist_name):
+        try:
+            user_id = self.sp.current_user()['id']
+            playlist = self.sp.user_playlist_create(user_id, playlist)
+            return playlist['id']
+        except SpotifyException as e:
+            print(f"Spotify API Error: {e}")
+            return None
+        except Exception as e:
+            print(f"Unexpected error occured: {e}")
+            return None
+        
+    def add_track_to_playlist(self, playlist)
